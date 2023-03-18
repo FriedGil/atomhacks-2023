b60abb33e6b2a74a2b788cb3907e47bc7b343568
Boris <khanikaeb@bxscience.edu>
2023-03-18 13:48:52 -0400

    ff

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -10,40 +10,22 @@
       Row,
       Column,
     } from "carbon-components-svelte";
-    import Automater from "../../components/Automater.svelte";
+    import Automater from "../components/Automater.svelte";
   
     let isSideNavOpen = false;
+
+    import { FileUploaderButton } from "carbon-components-svelte";
+
+    import { ContentSwitcher, Switch } from "carbon-components-svelte";
+
   </script>
 
-  <Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+
+<Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
     <svelte:fragment slot="skip-to-content">
       <SkipToContent />
     </svelte:fragment>
     <HeaderNav>
-  import { ExpandableTile } from "carbon-components-svelte";
-  import {
-  Header,
-  HeaderNav,
-  HeaderNavItem,
-  HeaderNavMenu,
-  SkipToContent,
-  Content,
-  Grid,
-  Row,
-  Column,
-} from "carbon-components-svelte";
-let isSideNavOpen = false;
-</script>
-
-<Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
-  <svelte:fragment slot="skip-to-content">
-    <SkipToContent />
-  </svelte:fragment>
-  <HeaderNav>
-    <HeaderNavItem href="/" text="Link 1" />
-    <HeaderNavItem href="/" text="Link 2" />
-    <HeaderNavItem href="/" text="Link 3" />
-    <HeaderNavMenu text="Menu">
       <HeaderNavItem href="/" text="Link 1" />
       <HeaderNavItem href="/" text="Link 2" />
       <HeaderNavItem href="/" text="Link 3" />
@@ -54,29 +36,20 @@ let isSideNavOpen = false;
       </HeaderNavMenu>
     </HeaderNav>
   </Header>
-
+  
   <Content>
     <Grid>
       <Row>
         <Column>
-          <Automater/>
+            <FileUploaderButton labelText="Add files" style="margin-bottom: 15px" />
         </Column>
+      </Row>
+      <Row>
+        <ContentSwitcher selectedIndex={1}>
+            <Switch text="Latest news" />
+            <Switch text="Trending" />
+            <Switch text="Recommended" />
+          </ContentSwitcher>
       </Row>
     </Grid>
   </Content>
-    </HeaderNavMenu>
-  </HeaderNav>
-</Header>
-
-<Content style="background-color: #f4f8fb">
-  <Grid>
-    <Row>
-      <Column>
-              <div style="height: 3rem; font-size: 24px; ">About</div>
-              <div style="background-color: white; height: 10rem; width: 60%; display:inline-block"></div>
-              <div style="height: 10rem; font-size: 16px; background-color: #101e33; height: 10rem; width: 38%; 
-                display:inline-block; float: right;"> </div>
-      </Column>
-    </Row>
-  </Grid>
-</Content>
--- project/src/routes/about/+page.svelte
+++ project/src/routes/about/+page.svelte
@@ -35,9 +35,9 @@ let isSideNavOpen = false;
     <Row>
       <Column>
               <div style="height: 3rem; font-size: 36px; font-weight: bold;">About</div>
-              <div style="background-color: white; height: 10rem; width: 60%; display:inline-block; border: 5px solid; border-color: #ffffff #b2d6a3 #b2d6a3 #fff"></div>
+              <div style="background-color: white; height: 10rem; width: 60%; display:inline-block; border: 2px solid; border-color: #f2fff8 "></div>
               <div style="height: 10rem; font-size: 16px; background-color: #101e33; height: 10rem; width: 38%; 
-                display:inline-block; float: right; border: 5px solid; border-color: #101e33 #b2d6a3 #b2d6a3 #101e33"> </div>
+                display:inline-block; float: right; border: 2px solid; border-color: #f2fff8 "> </div>
       </Column>
     </Row>
   </Grid>
