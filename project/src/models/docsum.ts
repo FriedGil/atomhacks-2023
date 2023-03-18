6afa49a865642116ef5b21266fe543dc9df2d719
Gil <friedmang@bxscience.edu>
2023-03-18 12:33:14 -0400

    key

---
+++ project/src/models/docsum.ts
@@ -0,0 +1,13 @@
+import type { Document } from "langchain/document";
+import { OpenAI } from "langchain/llms";
+import { loadSummarizationChain, AnalyzeDocumentChain} from "langchain/chains";
+
+
+export async function docsum(docs:Document[]): Promise<string>{
+    const model = new OpenAI({ openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY, temperature: 0.9 });
+    const chain = loadSummarizationChain(model);
+    const res = await chain.call({
+    input_documents: docs,
+    });
+    return res.value.toString();
+}
