b8996eb1d1f1387a05f7abe1f45729c2b4a21b10
Boris <khanikaeb@bxscience.edu>
2023-03-18 10:34:51 -0400

    yuhh

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<script>
-    console.log(import.meta.env.OPENAI_API_KEY)
-</script>
+{process.env.OPENAI_API_KEY}
