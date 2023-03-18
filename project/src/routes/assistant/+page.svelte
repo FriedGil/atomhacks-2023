5180eef944b351339c17935fcdaab74d4c042799
Boris <khanikaeb@bxscience.edu>
2023-03-18 16:02:56 -0400

    frndd

--- project/src/routes/assistant/+page.svelte
+++ project/src/routes/assistant/+page.svelte
@@ -21,6 +21,8 @@
 
     import { ButtonSet, Button } from "carbon-components-svelte";
 
+    import { CodeSnippet } from "carbon-components-svelte";
+
   </script>
 
 
@@ -29,9 +31,9 @@
       <SkipToContent />
     </svelte:fragment>
     <HeaderNav>
-      <HeaderNavItem href="/" text="Home" />
-      <HeaderNavItem href="/" text="DWGB" />
-      <HeaderNavItem href="/" text="About" />
+      <HeaderNavItem href="https://atomhacks-2023.vercel.app/" text="Home" />
+      <HeaderNavItem href="https://atomhacks-2023.vercel.app/assistant" text="DWGB" />
+      <HeaderNavItem href="https://atomhacks-2023.vercel.app/about" text="About" />
     </HeaderNav>
   </Header>
   
@@ -41,23 +43,29 @@
         <Column>
           <h1 style="margin-bottom: 25px; font-weight: bold">Upload a file!</h1>
             <Tabs style="margin-top: 15px">
-              <Tab label="Q&A" />
+              <Tab label="Q&A"/>
               <Tab label="Summarisation" />
               <svelte:fragment slot="content">
                 <TabContent style="width: 100%">
                   <FileUploaderButton labelText="Add files" />
-                  <TextInput labelText="" placeholder="Ask your question..." style="width: 100%; float: right; margin-top: 15px; margin-bottom: 15px;" />
+                  <TextInput labelText="" placeholder="Or enter sample text..." style="width: 100%; float: right; margin-top: 15px" />
+                  <TextInput labelText="" placeholder="Ask your question..." style="width: 50%; float: right; margin-top: 15px; margin-bottom: 15px;" />
                   <Button kind="danger-tertiary">Cancel</Button>
                   <Button>Submit</Button>
-                  <p style="margin-top: 15px">
-                    "output text here"
-                  </p>
+                  <h3 style="margin-top: 15px">
+                    Output:
+                    <CodeSnippet code="text" style="margin-top: 15px"/>
+                  </h3>
                 </TabContent>
                 <TabContent>
                   <FileUploaderButton labelText="Add files" />
-                <p style="margin-top: 15px">
-                  "output text here"
-                </p>
+                  <TextInput labelText="" placeholder="Or enter your text..." style="width: 100%; float: right; margin-top: 15px; margin-bottom: 15px;" />
+                  <Button kind="danger-tertiary">Cancel</Button>
+                  <Button>Submit</Button>
+                  <h3 style="margin-top: 15px">
+                    Output:
+                    <CodeSnippet code="text" style="margin-top: 15px"/>
+                  </h3>
                 </TabContent>
               </svelte:fragment>
             </Tabs>
