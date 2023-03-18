1581f9205a8b2ceef69d8f0a8dcf0cacd842e6bc
Gil <friedmang@bxscience.edu>
2023-03-18 11:22:54 -0400

    this works

--- project/src/components/main.js
+++ project/src/components/main.js
@@ -2,9 +2,4 @@
 import { OpenAI } from "langchain/llms";
 import { TextLoader } from "langchain/document_loaders";
 
-async function loader(path){
-    const loader = new TextLoader(path);
-    const docs = await loader.load();
-    console.log({ docs });
-}
 
--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -1,4 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { OpenAI } from "langchain/llms";
+    import { loadSummarizationChain } from "langchain/chains";
+    import { AnalyzeDocumentChain } from "langchain/chains";
 
-{process.env.OPENAI_API_KEY}
+    async function t(){
+        const text = "Hello World. You are looking very worldy today"
+        const model = new OpenAI({ openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY, temperature: 0.9 });
+        /** Load the summarization chain. */
+        const combineDocsChain = loadSummarizationChain(model);
+        /** Pass this into the AnalyzeDocumentChain. */
+        const chain = new AnalyzeDocumentChain({
+        combineDocumentsChain: combineDocsChain,
+        });
+        const res = await chain.call({
+        input_document: text,
+        });
+        console.log(res);
+        return res;
+    }
+
+
+</script>
+<h1 style="">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#await t()}
+	<p>...waiting</p>
+{:then res}
+	<p>The number is {res}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
+
+
